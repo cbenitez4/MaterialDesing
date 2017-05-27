c1543ae93ace9986169dbe59c744c515eec79818
Roberto <Roberto@morales.com.co>
2017-05-27 11:34:44 -0500

    configuracion Persona post

--- app/src/main/java/com/example/labsoftware1/personamaterial/Persona.java
+++ app/src/main/java/com/example/labsoftware1/personamaterial/Persona.java
@@ -2,7 +2,22 @@ package com.example.labsoftware1.personamaterial;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.UUID;
 
 /**
@@ -88,9 +103,112 @@ public class Persona {
         this.idfoto = idfoto;
     }
 
+    public void guardarRemoto(final Context contexto){
+        new AsyncTask<Void, Void, String>(){
+
+            @Override
+            protected String doInBackground(Void... params) {
+                HttpURLConnection conexion = null;
+
+                try {
+                    URL url = new URL("http://54.175.250.214/guardar.php");
+                    conexion =(HttpURLConnection)url.openConnection();
+                    conexion.setConnectTimeout(30000);
+                    conexion.setReadTimeout(30000);
+
+                    //Configuracion de envío de datos via POST
+                    conexion.setRequestMethod("POST");
+                    conexion.setDoOutput(true);
+                    conexion.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+
+                    //Crear consulta con los datos
+
+                    StringBuilder builder = new StringBuilder();
+                    builder.append("id");
+                    builder.append("=");
+                    builder.append(URLEncoder.encode(uuid,"UTF-8"));
+                    builder.append("&");
+                    builder.append("foto");
+                    builder.append("=");
+                    builder.append(URLEncoder.encode(urlfoto,"UTF-8"));
+                    builder.append("&");
+                    builder.append("cedula");
+                    builder.append("=");
+                    builder.append(URLEncoder.encode(cedula,"UTF-8"));
+                    builder.append("&");
+                    builder.append("nombre");
+                    builder.append("=");
+                    builder.append(URLEncoder.encode(nombre,"UTF-8"));
+                    builder.append("&");
+                    builder.append("apellido");
+                    builder.append("=");
+                    builder.append(URLEncoder.encode(apellido,"UTF-8"));
+                    builder.append("&");
+                    builder.append("idfoto");
+                    builder.append("=");
+                    builder.append(URLEncoder.encode(idfoto,"UTF-8"));
+
+                    String query = builder.toString();
+
+                    conexion.setFixedLengthStreamingMode(query.getBytes("UTF-8").length);
+
+                    OutputStream outputStream = conexion.getOutputStream();
+                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream,"UTF-8");
+                    BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                    bufferedWriter.write(query);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+
+                    //Conectar
+                    conexion.connect();
+
+                    //Leer Respuesta del servidor
+
+                    InputStream inputStream = conexion.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"UTF-8");
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    StringBuilder datos = new StringBuilder();
+                    String linea;
+                    while((linea =bufferedReader.readLine())!=null){
+                        datos.append(linea);
+                    }
+
+                    bufferedReader.close();
+                    return datos.toString();
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return null;
+
+            }
+
+            @Override
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    boolean success = jsonObject.getBoolean("success");
+                    if(success){
+                        urlfoto = jsonObject.getString("urlfoto");
+                        guardarLocal(contexto);
+
+                    }
 
 
-    public void guardar(Context contexto){
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.execute();
+    }
+
+
+    public void guardarLocal(Context contexto){
         //declarar las variables
         SQLiteDatabase db;
         String sql;
